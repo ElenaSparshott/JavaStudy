f34d11a7a6bd4d9edf848ecca325cf2c33a22c83
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2023-03-09 22:47:39 +0000

    hello

--- src/MyClass.java
+++ src/MyClass.java
@@ -41,7 +41,15 @@ public class MyClass {
     }
 
 
+    public class HelloWorld {
+        // Write a public static function `greet` that returns "hello world!"
+        public static String greet () {
+            return "hello world!";
+        }
+    }
+
 }
+
 
 
 
